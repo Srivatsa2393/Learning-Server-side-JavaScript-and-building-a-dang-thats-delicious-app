fa78436c8a39c1f59667ab9ba1d4384e2101faa6
Srivatsa S <srivatsashankaran@gmail.com>
2017-05-23 23:44:25 +0530

    Middleware and Error handling

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,3 +1,13 @@
+exports.myMiddleware = (req,res,next) => {
+  req.name = 'S ri';
+  if(req.name === 'Sri'){
+    throw Error('This is a stupid name');
+  }
+  next();
+};
+
+
 exports.homePage = (req,res) => {
+  console.log(req.name);
   res.render('index')
 }
