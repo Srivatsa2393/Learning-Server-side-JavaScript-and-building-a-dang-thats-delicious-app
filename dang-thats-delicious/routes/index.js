866b5fbca85a5640deb5ca16a0e779d3be53af49
Srivatsa S <srivatsashankaran@gmail.com>
2017-05-23 22:54:47 +0530

    Controllers and MVC PAttern

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,24 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  // console.log('Hey!!!')
-  const sri = { name: 'Srivatsa', age: 24, color: 'white', cool: true};
-  // res.json(sri);
-  // res.send('Hey! It works!');
-  //res.send(req.query.name);
-  //res.json(req.query);
-  res.render('hello',{
-    name:'sri',
-    dog: req.query.dog,
-    title: 'I love food'
-  });
-});
+router.get('/', storeController.homePage);
 
-router.get('/reverse/:name', (req,res) => {
-  const reverse = [...req.params.name].reverse().join('')
-  res.send(reverse);
-});
+// router.get('/reverse/:name', (req,res) => {
+//   const reverse = [...req.params.name].reverse().join('')
+//   res.send(reverse);
+// });
 
 module.exports = router;
