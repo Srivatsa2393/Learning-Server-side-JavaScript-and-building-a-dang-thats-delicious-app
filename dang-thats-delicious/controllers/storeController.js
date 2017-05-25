b6c412a1c0dc777c34a06238e60cc8d0c3c56f35
Srivatsa S <srivatsashankaran@gmail.com>
2017-05-25 22:20:48 +0530

    Using Asyncawait()

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -6,6 +6,8 @@
 //   next();
 // };
 
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store')
 
 exports.homePage = (req,res) => {
   console.log(req.name);
@@ -17,7 +19,11 @@ exports.addStore = (req,res) => {
   res.render('editStore', { title : 'Add Store'})
 }
 
-exports.createStore = (req,res) => {
-  console.log(req.body);
-  res.json(req.body);
+exports.createStore = async (req,res) => {
+  // console.log(req.body);
+  // res.json(req.body);
+  const store = new Store(req.body);
+  await store.save();
+  //console.log('It works');
+  res.redirect('/');
 }
