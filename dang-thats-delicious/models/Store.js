9b6236e8a820dbed0db6360cc064b2c7f97d783b
Srivatsa S <srivatsashankaran@gmail.com>
2017-05-28 16:47:54 +0530

    pre-save hooks to make new slugs

--- dang-thats-delicious/models/Store.js
+++ dang-thats-delicious/models/Store.js
@@ -36,12 +36,19 @@ const storeSchema  = new mongoose.Schema({
   photo: String
 });
 
-storeSchema.pre('save', function(next){
+storeSchema.pre('save', async function(next){
   if(!this.isModified('name')){
     next();//skip it
     return;//stop this function from running
   }
   this.slug = slug(this.name);
+  //find other stores that have a slug of storename1,storename2,storename3..
+  const slugRegEx= new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx })
+
+  if(storesWithSlug.length){
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
   next();
   //TODO make more resiliant so slugs are unique
 });
