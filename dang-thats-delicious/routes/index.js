52af6cacbaa58b33c9d43854cdf8b148eef5e7e0
Srivatsa S <srivatsashankaran@gmail.com>
2017-05-29 13:33:45 +0530

    Saving registered users to the database and logging them in using Passport.js

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers.js');
 
 // Do work here
@@ -37,7 +38,11 @@ router.get('/register', userController.registerForm);
 //2. register the user
 //3. we need to log them in
 
-router.post('/register', userController.validateRegister);
+router.post('/register',
+  userController.validateRegister,
+  userController.register,
+  authController.login
+);
 
 // router.get('/reverse/:name', (req,res) => {
 //   const reverse = [...req.params.name].reverse().join('')
